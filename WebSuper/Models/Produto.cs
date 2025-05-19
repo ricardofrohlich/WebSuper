edba0ec3cb69f86153e57940d91d28655175b3fd
ricardofrohlich <ricardosma@gmail.com>
2025-05-19 19:34:49 -0300

    as

--- WebSuper/Models/Produto.cs
+++ WebSuper/Models/Produto.cs
@@ -1,8 +1,12 @@
-﻿namespace WebSuper.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSuper.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
+        [StringLength(30, ErrorMessage="A descrição deve ter até 30 caracteres")]
         public string Descricao { get; set; }
         public double Preco { get; set; }
 
